311df1b1e4f297cf7b812616dc77df6efcf3ad3f
imdragon <i.m.dragon@gmail.com>
2016-08-16 17:48:48 -0700

    Left start for next and previous buttons Updates gradle and AS

--- app/src/main/java/com/sam_chordas/android/stockhawk/widget/stockWidget.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/widget/stockWidget.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.util.Log;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import com.sam_chordas.android.stockhawk.R;
@@ -17,6 +18,7 @@ import com.sam_chordas.android.stockhawk.ui.MyStocksActivity;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
@@ -28,6 +30,7 @@ import java.util.TimeZone;
 public class stockWidget extends AppWidgetProvider {
 
     private static String TAG = "stockWidget";
+    public static ArrayList<String> currentStocks;
 
     // TODO: 8/9/2016 see if moving to update will fix widget update
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
@@ -60,9 +63,11 @@ public class stockWidget extends AppWidgetProvider {
             if (tempCursor.getString(tempCursor.getColumnIndex("is_current")).equalsIgnoreCase("0"))
                 ;
             {
+
                 sb.append(tempCursor.getString(tempCursor.getColumnIndex("symbol")));
                 sb.append(" ");
                 sb.append("$" + tempCursor.getString(tempCursor.getColumnIndex("bid_price")));
+                currentStocks.add(sb.toString());
                 tempCursor.moveToNext();
                 if (i < tempCursor.getCount() - 1) {
                     sb.append("\n");
@@ -81,6 +86,8 @@ public class stockWidget extends AppWidgetProvider {
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
 
+        views.getLayoutId();
+// TODO: 8/16/2016 look into making a button on widget open app instead of entire widget face, that way next and previous may work
         // Set up on tap widget
         Intent mIntent = new Intent(context, MyStocksActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, mIntent, 0);
@@ -106,5 +113,10 @@ public class stockWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    public void widgetPreviousButton (View v){
+
+    }
+
 }
 
