5e6dfe584655c1b5cd7beeb66574ba7235811549
imdragon <i.m.dragon@gmail.com>
2016-08-28 22:26:32 -0700

    Extracted chosen data from JSON response for historical data

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetails.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetails.java
@@ -3,11 +3,15 @@ package com.sam_chordas.android.stockhawk.ui;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.sam_chordas.android.stockhawk.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
@@ -45,6 +49,9 @@ public class StockDetails extends Activity {
     class GetHistoricalData extends AsyncTask<String, Void, Void> {
 
         public StringBuilder total = new StringBuilder();
+        public StringBuilder quoteString = new StringBuilder();
+        private JSONObject results;
+        private JSONArray quotes;
 
         /**
          * Override this method to perform a computation on a background thread. The
@@ -62,11 +69,12 @@ public class StockDetails extends Activity {
          */
         @Override
         protected Void doInBackground(String... stockSymbol) {
+            Log.e("StockDetails Async", "doinBackground Running");
             URL url = null;
             try {
 //                https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22tsla%22%20and%20startDate%20%3D%20%222009-09-11%22%20and%20endDate%20%3D%20%222010-03-10%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
 
-                url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22"+stockSymbol[0]+"%22%20and%20startDate%20%3D%20%222016-01-01%22%20and%20endDate%20%3D%20%222016-02-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
+                url = new URL("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.historicaldata%20where%20symbol%20%3D%20%22" + stockSymbol[0] + "%22%20and%20startDate%20%3D%20%222016-01-01%22%20and%20endDate%20%3D%20%222016-02-01%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
@@ -93,6 +101,29 @@ public class StockDetails extends Activity {
                 e.printStackTrace();
             }
 
+//            JSON processing
+
+            try {
+                results = new JSONObject(total.toString());
+
+                results = results.getJSONObject("query");
+                results = results.getJSONObject("results");
+                quotes = results.getJSONArray("quote");
+                Log.e("results are", quotes.toString());
+//                quotes = results.getJSONArray("quote");
+                quoteString.append("Something else but this");
+                for (int i = 0; i < quotes.length(); i++) {
+                    Log.e("StockDetails Async", "Loop ran: "+i+" times");
+                    quoteString.append("\n ***** \n");
+                    JSONObject newQuote = quotes.getJSONObject(i);
+                    quoteString.append(newQuote.getString("Date"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+//
 
             return null;
         }
@@ -110,7 +141,9 @@ public class StockDetails extends Activity {
          */
         @Override
         protected void onPostExecute(Void aVoid) {
-stockLabel.setText(total.toString());
+
+//            stockLabel.setText(total.toString());
+            stockLabel.setText(quoteString.toString());
         }
     }
 }
